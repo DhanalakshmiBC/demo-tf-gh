4fd81f8eadb1854a5cee44272f2a64cdfb3b81bc
Deepika <deepika@Deepikas-MacBook-Air.local>
2025-07-10 21:04:59 +0530

    old

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-061ad72bc140532fd" # Replace with a valid AMI ID for us-west-2
+  ami           = "ami-05ee755be0cd7555c" # Replace with a valid AMI ID for us-west-2
   instance_type = "t2.micro"
   security_groups = [aws_security_group.demo_sg.id]
   subnet_id= aws_subnet.main_subnet.id
